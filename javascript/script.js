e350d01361aac1202d7f1b0077a5fc3f7ee5917d
Olivier-Kango <olivierkango@gmail.com>
2023-05-30 22:34:53 +0300

    Fix linter error

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,14 @@
 // DESKTOP VERSION
 
-document.addEventListener('DOMContentLoaded', function() {
-  const navbar = document.querySelector("#sticky-nav");
-  let top = navbar.offsetTop;
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.querySelector('#sticky-nav');
+  const top = navbar.offsetTop;
   const section = document.querySelector('#history');
 
   function stickynavbar() {
     if (window.scrollY >= top) {
       navbar.classList.add('sticky');
-      section.style.marginTop = navbar.offsetHeight + 'px';
+      section.style.marginTop = `${navbar.offsetHeight}px`;
     } else {
       navbar.classList.remove('sticky');
       section.style.marginTop = 0;
@@ -18,16 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
   window.addEventListener('scroll', stickynavbar);
 
   navbar.style.transition = 'top 0.3s ease';
-  
+
   stickynavbar();
 });
 
 // Automatic navigation
 let counter = 1;
-setInterval(function() {
-  document.getElementById('radio' + counter).checked = true;
-  counter++;
-  if(counter > 3) {
+setInterval(() => {
+  document.getElementById(`radio${counter}`).checked = true;
+  counter += 1;
+  if (counter > 3) {
     counter = 1;
   }
 }, 4000);
@@ -60,18 +60,18 @@ scrollTopButton.addEventListener('click', (event) => {
 
 // MOBILE VERSION
 
-// Humberger 
-document.addEventListener('DOMContentLoaded', function() {
-  let hamburgerIcon = document.getElementById('hamburger-icon');
-  let navList = document.querySelector('.x');
-  let bars = document.querySelectorAll('#hamburger-icon .bar');
+// Humberger
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const navList = document.querySelector('.x');
+  const bars = document.querySelectorAll('#hamburger-icon .bar');
 
-  hamburgerIcon.addEventListener('click', function() {
+  hamburgerIcon.addEventListener('click', () => {
     navList.classList.toggle('show');
 
     // X cross
-    for(let i = 0; i < bars.length; i++) {
+    for (let i = 0; i < bars.length; i += 1) {
       bars[i].classList.toggle('change');
-    }    
+    }
   });
 });
