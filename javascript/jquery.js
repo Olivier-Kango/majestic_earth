252ace3162233703d269f68f3f62ba503dd6b3dd
Olivier-Kango <olivierkango@gmail.com>
2023-06-02 21:40:30 +0300

    Delete Psalms

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -5,51 +5,51 @@ $(document).ready(() => {
   const array = [
     {
       bgpopup1: "url('./assets/images/1.jpg')",
-      text1: 'I will speak of the glorious honour of thy majesty, and of thy wondrous works. And men shall speak of the might of thy terrible acts: and I will declare thy greatness. Psalm 145:5-6',
+      text1: 'I will speak of the glorious honour of thy majesty, and of thy wondrous works. And men shall speak of the might of thy terrible acts: and I will declare thy greatness.',
     },
     {
       bgpopup2: "url('./assets/images/2.jpg')",
-      text2: 'O LORD, how manifold are thy works! in wisdom hast thou made them all: the earth is full of thy riches. Psalm 104:24',
+      text2: 'O LORD, how manifold are thy works! in wisdom hast thou made them all: the earth is full of thy riches.',
     },
     {
       bgpopup3: "url('./assets/images/3.jpg')",
-      text3: 'In his hand are the deep places of the earth: the strength of the hills is his also. The sea is his, and he made it: and his hands formed the dry land. Psalm 95:4-5',
+      text3: 'In his hand are the deep places of the earth: the strength of the hills is his also. The sea is his, and he made it: and his hands formed the dry land.',
     },
     {
       bgpopup4: "url('./assets/images/4.jpg')",
-      text4: "From the rising of the sun unto the going down of the same the LORD's name is to be praised. Psalm 113:3",
+      text4: "From the rising of the sun unto the going down of the same the LORD's name is to be praised.",
     },
     {
       bgpopup5: "url('./assets/images/5.jpg')",
-      text5: 'Bless the LORD, O my soul. O LORD my God, thou art very great; thou art clothed with honour and majesty. Psalm 104:1',
+      text5: 'Bless the LORD, O my soul. O LORD my God, thou art very great; thou art clothed with honour and majesty.',
     },
     {
       bgpopup6: "url('./assets/images/6.jpg')",
-      text6: 'Tremble, thou earth, at the presence of the Lord, at the presence of the God of Jacob; Which turned the rock into a standing water, the flint into a fountain of waters. Psalm 114:7-8',
+      text6: 'Tremble, thou earth, at the presence of the Lord, at the presence of the God of Jacob; Which turned the rock into a standing water, the flint into a fountain of waters.',
     },
     {
       bgpopup7: "url('./assets/images/7.jpg')",
-      text7: 'They also that dwell in the uttermost parts are afraid at thy tokens: thou makest the outgoings of the morning and evening to rejoice. Psalm 65:8',
+      text7: 'They also that dwell in the uttermost parts are afraid at thy tokens: thou makest the outgoings of the morning and evening to rejoice.',
     },
     {
       bgpopup8: "url('./assets/images/8.jpg')",
-      text8: 'Let all the earth fear the LORD: let all the inhabitants of the world stand in awe of him. For he spake, and it was done; he commanded, and it stood fast. Psalm 33:8-9',
+      text8: 'Let all the earth fear the LORD: let all the inhabitants of the world stand in awe of him. For he spake, and it was done; he commanded, and it stood fast.',
     },
     {
       bgpopup9: "url('./assets/images/9.jpg')",
-      text9: 'When I consider thy heavens, the work of thy fingers, the moon and the stars, which thou hast ordained; What is man, that thou art mindful of him? and the son of man, that thou visitest him? Psalm 8:3-4',
+      text9: 'When I consider thy heavens, the work of thy fingers, the moon and the stars, which thou hast ordained; What is man, that thou art mindful of him? and the son of man, that thou visitest him?',
     },
     {
       bgpopup10: "url('./assets/images/10.jpg')",
-      text10: 'They also that dwell in the uttermost parts are afraid at thy tokens: thou makest the outgoings of the morning and evening to rejoice. Psalm 65:8',
+      text10: 'They also that dwell in the uttermost parts are afraid at thy tokens: thou makest the outgoings of the morning and evening to rejoice.',
     },
     {
       bgpopup11: "url('./assets/images/11.jpg')",
-      text11: 'For the LORD is a great God, and a great King above all gods. Psalm 95:3',
+      text11: 'For the LORD is a great God, and a great King above all gods.',
     },
     {
       bgpopup12: "url('./assets/images/12.jpg')",
-      text12: 'Thou visitest the earth, and waterest it: thou greatly enrichest it with the river of God, which is full of water: thou preparest them corn, when thou hast so provided for it. Psalm 65:9',
+      text12: 'Thou visitest the earth, and waterest it: thou greatly enrichest it with the river of God, which is full of water: thou preparest them corn, when thou hast so provided for it.',
     },
   ];
 
