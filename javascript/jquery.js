6c89ca51196ee66b4d793a6cec6a5625e574100d
Olivier-Kango <olivierkango@gmail.com>
2023-07-04 19:39:24 +0300

    Remove a link

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -175,4 +175,9 @@ $(document).ready(() => {
   };
 
   // Mounts Selection
+  $('.mountain-link').click(() => {
+    const paragraphIndex = $(this).data('paragraph');
+    $('#schedule p').removeClass('para-bold');
+    $(`#schedule p:nth-child(${paragraphIndex})`).addClass('para-bold');
+  });
 });
